ff09ee1d883817869a9a92162ca53a382ae61af5
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-01-04 02:04:06 +0530

    app

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify, render_template,send_file, session
+import requests
+import io
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+PORT = os.getenv('PORT', 5000)
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+if not YOUTUBE_API_KEY:
+    raise ValueError("No YOUTUBE_API_KEY set for Flask application")
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/download/<quality>')
+def download_image(quality):
+    url = session.get(f"thumbnail_{quality}")
+    if url:
+        response = requests.get(url)
+        return send_file(
+            io.BytesIO(response.content),
+            mimetype='image/jpeg',
+            as_attachment=True,
+            download_name=f'youtube-thumbnail-{quality}.jpg'
+        )
+@app.route('/', methods=['GET', 'POST'])
+def link():
+    if request.method == 'POST':
+        link = request.form['link']
+        video_id = link.split('v=')[1]
+        response = requests.get(
+            f'https://www.googleapis.com/youtube/v3/videos?part=snippet&id={video_id}&key={YOUTUBE_API_KEY}')
+        data = response.json()
+        thumbnails = data['items'][0]['snippet']['thumbnails']
+        thumbnail_urls = {
+            'default': thumbnails['default']['url'],
+            'high': thumbnails['high']['url'],
+            'maxres': thumbnails['maxres']['url'],
+        }
+        session['thumbnail_default'] = thumbnails['default']['url']
+        session['thumbnail_high'] = thumbnails['high']['url']
+        session['thumbnail_maxres'] = thumbnails['maxres']['url']
+        
+        return render_template('index.html', thumbnails=thumbnail_urls)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=PORT)
